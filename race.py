3a572263046fc541f63c97b472a828864189ebbe
betoelmanco123 <robert.rom.es@icloud.com>
2026-04-06 21:49:15 -0600

    functional neurons

--- race.py
+++ race.py
@@ -4,7 +4,7 @@ import processIm
 CONVERT = math.pi / 180
 
 validMap = processIm.get_matrix("sprites/map3.jpg")
-startposition = (750, 100)
+startposition = (550, 50)
 
 
 class RaceCar:
@@ -15,45 +15,43 @@ class RaceCar:
         self.startposition = startposition
         self.position = startposition
         self.x, self.y = self.position
-        self.speed = 1
-        self.aceleration = 0.1
-        self.mutateSpeed = 0.1
-        self.turnSpeed = 12
+        self.speed = 3
+        self.aceleration = 0.09
+        self.mutateSpeed = 0.09
+        self.turnSpeed = 1.8
         self.alive = True
         self.direction = 180
         self.distanceTraveled = 0
         self.hitbox = [13, 28]
-        self.speedLimit = 5
+        self.speedLimit = 10
         self.steering = 0
-        self.length = 10
+        self.length = 28
+        self.known = list()
+        self.reward = 0
 
         # map stats
         self.map = validMap
         self.checkpoints = [
-            (145, 145),
-            (145, 750),
+            (85, 85),
+            (100, 400),
+            (100, 750),
+            (450, 750),
             (750, 750),
-            (750, 145),
-
+            (820, 425),
+            (800, 100),
         ]
         self.laps = 0
         self.counterCheckpoint = 0
 
         # Raycast
-        self.RayLarge = 200
+        self.RayLarge = 300
         self.RayValues = [0 for _ in range(5)]
 
         # Neurons
-        self.hidden_size = 7
-        self.input_size = 7
-        self.output_size = 2
-        self.angles = [
-            -90,
-            -45,
-            0,
-            45,
-            90
-        ]
+        self.hidden_size = 12
+        self.input_size = 9
+        self.output_size = 3
+        self.angles = [-90, -45, 0, 45, 90]
         self.w_ih = [
             [random.uniform(-1, 1) for _ in range(self.input_size)]
             for _ in range(self.hidden_size)
@@ -67,61 +65,68 @@ class RaceCar:
     def acelerate(self):
         self.speed += self.aceleration
 
-    def updatePosition(self, steering=0, accel=0):
+    def updatePosition(self, steering=0, accel=0, time=0):
         if not self.alive:
             return
-        self.speed += accel
-        self.speed = min(self.speedLimit, max(0, self.speed))
-        self.direction += (self.speed / self.length) * math.tan(steering)
+        self.speed += accel * time * 60
+        self.speed = min(self.speedLimit, max(-self.speedLimit, self.speed))
+        self.direction += steering * self.turnSpeed * (self.speed / self.speedLimit)
         self.direction %= 360
         rad = self.direction * (CONVERT)
         temp_x = self.x + self.speed * math.cos(rad)
         temp_y = self.y + self.speed * math.sin(rad)
 
         if temp_x >= len(self.map[0]) or temp_x < 0:
+            self.alive = False
             return False
         if temp_y >= len(self.map) or temp_y < 0:
+            self.alive = False
             return False
 
         if self.hitboxColittion(temp_x, temp_y):
             self.alive = False
             return False
 
-        if self.speed == 0:
-            self.alive = False
         self.distanceTraveled += math.hypot(temp_x - self.x, temp_y - self.y)
         self.x = temp_x
         self.y = temp_y
 
         self.upToDate()
 
+        if not self.position in self.known:
+            self.known.append(self.position)
+            self.reward += 1
+        else:
+            self.reward -= 1
         return True
 
     # AI
-    def think(self):
-
+    def think(self, time):
         inputs = [v / self.RayLarge for v in self.multipleRayCast()]
-        inputs.append(self.speed / 10)
-        inputs.append(self.direction)
+        inputs.append(self.speed / self.speedLimit)
+        inputs.append(math.sin(self.direction * CONVERT))
+        inputs.append(math.cos(self.direction * CONVERT))
+        inputs.append(time / 10)
 
         hidden = []
         for i in range(self.hidden_size):
             suma = 0
             for j in range(self.input_size):
                 suma += self.w_ih[i][j] * inputs[j]
-
             hidden.append(math.tanh(suma))
+
         outputs = []
         for i in range(self.output_size):
             suma = 0
             for j in range(self.hidden_size):
                 suma += self.w_ho[i][j] * hidden[j]
-
             outputs.append(math.tanh(suma))
 
-        turn = outputs[0]
-        accel = outputs[1]
+        accel = outputs[0]
+        right = outputs[1]
+        left = outputs[2]
 
+        turn = left - right
         return turn, accel
 
     # map
@@ -178,6 +183,7 @@ class RaceCar:
             for point in face:
                 sx, sy = point
                 if self.isWall(sx, sy):
+                    self.reward -= 100
                     return True
         return False
 
@@ -199,8 +205,10 @@ class RaceCar:
         child = RaceCar()
         child.w_ih = [row[:] for row in self.w_ih]
         child.w_ho = [row[:] for row in self.w_ho]
-        if random.random() <.70:
-            child.mutate(max(1.2 - (self.counterCheckpoint * .02 + self.laps * .09), .001))
+        if random.random() < 0.70:
+            child.mutate(
+                max(1.2 - (self.counterCheckpoint * 0.02 + self.laps * 0.09), 0.001)
+            )
         return child
 
     def mutate(self, speed=1):
@@ -238,16 +246,14 @@ class RaceCar:
             self.RayCast(self.direction),
             self.RayCast(self.direction + 45),
             self.RayCast(self.direction + 90),
-
         ]
         self.RayValues = values
         return values
 
     # execute one turn
-    def run(self):
-        turn, accel = self.think()
-        self.updatePosition(turn, accel)
-        self.speed *= 0.98
+    def run(self, time):
+        turn, accel = self.think(time)
+        self.updatePosition(turn, accel, time)
 
     # auxiliar
     def printPosition(self):
@@ -260,7 +266,7 @@ class RaceCar:
         print(f"Distance TRaveled {self.distanceTraveled}")
         print(f"the speed is {self.speed}")
         print(f"this car achived {self.laps} laps")
-    
+
     def print_laps(self):
         print(f"-------------------{self.laps}-----------")
         print(f"-------------------{self.counterCheckpoint}-----------")
@@ -268,10 +274,13 @@ class RaceCar:
     def goToStart(self):
         self.position = self.startposition
         self.x, self.y = self.position
+        self.speed = 5
         self.distanceTraveled = 0
         self.alive = True
         self.counterCheckpoint = 0
         self.laps = 0
+        self.known = list()
+        self.reward = 0
 
     def upToDate(self):
         self.position = (self.x, self.y)
@@ -316,10 +325,46 @@ def getGoal(mapa):
 def getwinners(cars, number):
     values = dict()
     for car in cars:
-        values[car] = 1000 * (car.laps * 4 + car.counterCheckpoint) + car.distanceTraveled
+
+        value = 1000 * (car.laps * 4 + car.counterCheckpoint) + car.distanceTraveled / 5
+        if not car.alive:
+            value -= 100
+        if car.speed <= 0.2:
+            value -= 500
+        if car.speed <= 0.15:
+            value -= 100
+        value += car.reward * 10
+
+        values[car] = value
+
     ordenado = sorted(values, key=values.get)
 
     return ordenado[-number:]
+
+
+def get_children_result(A, B):
+
+    child = RaceCar()
+    child.w_ih = [row[:] for row in A.w_ih[:6]] + [row[:] for row in B.w_ih[6:]]
+    child.w_ho = [row[:] for row in A.w_ho[:1]] + [row[:] for row in B.w_ho[1:]]
+    if random.random() < 0.50:
+        child.mutate()
+    return child
+
+
+def mix_up(cars, number):
+
+    new_ones = list()
+
+    counter = 0
+    while counter < number:
+
+        A = random.choice(cars)
+        B = random.choice(cars)
+        child = get_children_result(A, B)
+        new_ones.append(child)
+        counter += 1
+    return new_ones
 
 
 def getDistance(pointA, pointB):
