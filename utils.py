6af311c308980d91b468b34aaf7decd04e9ff1ea
betoelmanco123 <robert.rom.es@icloud.com>
2026-04-07 23:07:32 -0600

    improved fitness system

--- utils.py
+++ utils.py
@@ -1,24 +1,39 @@
 import math
 
 
+def calculate_fitness(car):
+    value = 0.0
+
+    value += car.laps * 1000
+    value += car.counterCheckpoint * 150
+
+    value += car.distanceTraveled * 0.2
+
+    if car.alive:
+        value += 250
+    else:
+        value -= 250
+
+    ideal_speed = 6
+    speed_error = abs(car.speed - ideal_speed)
+    value += max(0.0, 300 - speed_error * 120)
+
+    if car.counterCheckpoint == 0 and car.laps == 0:
+        value -= 500
+    
+    value += len(car.known) * 100
+
+    return value
+
+
 def getwinners(cars, number):
-    values = dict()
+    values = {}
+
     for car in cars:
+        values[car] = calculate_fitness(car)
 
-        value = 1000 * (car.laps * 4 + car.counterCheckpoint) + car.distanceTraveled / 2
-        if not car.alive:
-            value -= 100
-        if car.speed <= 0.2:
-            value -= 500
-        if car.speed <= 0.15:
-            value -= 100
-        value += car.reward * 100 * car.speed
-
-        values[car] = value
-
-    ordenado = sorted(values, key=values.get)
-
-    return ordenado[-number:]
+    ordenado = sorted(values, key=values.get, reverse=True)
+    return ordenado[:number]
 
 
 def getDistance(pointA, pointB):
