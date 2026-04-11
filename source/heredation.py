6d0355fc7fd85d82b990733772b6be9c200c35ff
betoelmanco123 <robert.rom.es@icloud.com>
2026-04-10 20:20:09 -0600

    stronger functions

--- source/heredation.py
+++ source/heredation.py
@@ -6,7 +6,7 @@ from source.utils import getwinners, calculate_fitness
 
 def get_children_result(A, B):
 
-    child = RaceCar()
+    child = RaceCar(validMap=A.map)
     child.w_ih = [row[:] for row in A.w_ih[:6]] + [row[:] for row in B.w_ih[6:]]
     child.w_ho = [row[:] for row in A.w_ho[:1]] + [row[:] for row in B.w_ho[1:]]
     if random.random() < 0.50:
@@ -34,25 +34,33 @@ def mix_up(cars, number):
 # simple mutation
 def get_childrens(cars, number):
     new = list()
-    temp = number // len(cars)
+    temp = (number // len(cars)) // 2 
     for i in cars:
+        for k in range(temp ):
+            child = i.haveChild(speed=.5)
+            new.append(child)
         for k in range(temp):
-            child = i.haveChild()
+            child = i.haveChild(speed=2)
             new.append(child)
     
     return new
 
 def clone_elite(car):
-    elite = RaceCar()
+    elite = RaceCar(validMap=car.map)
+
     elite.w_ih = [row[:] for row in car.w_ih]
     elite.w_ho = [row[:] for row in car.w_ho]
     elite.goToStart()
     return elite
 
-def new_generation(cars, selected=3, diversity=10):
+def new_generation(cars, selected=5, diversity=5, Nnew_generation=None, every=False):
     current = 0
     indicator = False
-    winners = getwinners(cars, selected)
+    if every:
+        winners = cars
+        print(winners)
+    else:
+        winners = getwinners(cars, selected)
     elite_cars = []
 
     for car in winners:
@@ -61,11 +69,14 @@ def new_generation(cars, selected=3, diversity=10):
         current = max(current, car.counterCheckpoint + car.laps * 6)
         elite = clone_elite(car)
         elite_cars.append(elite)
-
-    children_number = max(0, len(cars) - selected - diversity)
+    valid_map = car.map
+    if Nnew_generation is None:
+        children_number = max(0, len(cars) - selected - diversity)
+    else:
+        children_number = Nnew_generation
     new_cars = get_childrens(winners, children_number)
     for i in range(diversity):
-        car = RaceCar()
+        car = RaceCar(validMap=valid_map)
         new_cars.append(car)
 
     return new_cars + elite_cars, indicator, current
